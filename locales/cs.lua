c3d4c3d800ac67f2e6b62be553ca0a9620181d8a
Wex Scrips <wexscripts@gmail.com>
2024-12-15 19:09:21 +0100

    Add files via upload

---
+++ locales/cs.lua
@@ -0,0 +1,15 @@
+return {
+    mechanic = {
+        menuTitle = "Místní mechanik",
+        menuRepair = "Opravit vozidlo ($500)",
+        menuWash = "Umytí vozidla ($100)",
+        targetLabel = "Místní mechanik",
+        repairing = "Mechanik opravuje vozidlo...",
+        washing = "Mechanik myje vozidlo...",
+        repairSuccess = "Vozidlo bylo opraveno!",
+        washSuccess = "Vozidlo bylo umyto!",
+        notEnoughMoney = "Nemáš dostatek peněz!",
+        mustBeInVehicle = "Musíš sedět ve vozidle, abys mohl provádět akce mechanika!",
+        blipName = "Místní mechanik",
+    }
+}
---
+++ locales/en.lua
@@ -0,0 +1,15 @@
+return {
+    mechanic = {
+        menuTitle = "NPC Mechanic",
+        menuRepair = "Repair vehicle ($500)",
+        menuWash = "Wash vehicle ($100)",
+        targetLabel = "NPC Mechanic",
+        repairing = "The mechanic is repairing the vehicle...",
+        washing = "The mechanic is washing the vehicle...",
+        repairSuccess = "Vehicle repaired!",
+        washSuccess = "Vehicle washed!",
+        notEnoughMoney = "You don't have enough money!",
+        mustBeInVehicle = "You must be in a vehicle to use the mechanic!",
+        blipName = "NPC Mechanic",
+    }
+}
